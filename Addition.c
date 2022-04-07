2b05903078a1f80d91140fb651eedd4ebef4b4ca
PiKa919 <96786190+PiKa919@users.noreply.github.com>
2022-04-08 02:51:28 +0530

    Add files via upload

---
+++ Addition.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+    int a,b;
+    printf("Enter the value of a,b");
+    scanf("%d%d",&a,&b);
+    printf("Addition of a,b is %d",a+b);
+    getch();
+}
